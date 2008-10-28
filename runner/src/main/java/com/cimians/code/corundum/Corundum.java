a16a6f41bbc14085a214fb6caffa7f7ed2a1182c
Mat Schaffer <schapht@gmail.com>
2008-10-28 13:57:13 -0400

    Added code for loading files from classloader as a comment.  Eventually I'd like to implement this as a support library loaded through the Runner class.

--- runner/src/main/java/com/cimians/code/corundum/Corundum.java
+++ runner/src/main/java/com/cimians/code/corundum/Corundum.java
@@ -11,6 +11,30 @@ import org.jruby.RubyRuntimeAdapter;
 import org.jruby.javasupport.JavaEmbedUtils;
 
 public class Corundum {
+  /*
+    # Loader example
+    class ResourceLoader
+
+      def read(path)
+        path = "#{BASE_RESOURCE_PATH}/#{path}"
+        is = resource.get_resource_as_stream path
+        bis = java.io.BufferedInputStream.new(is)
+        bytes = ''
+        while (byte = bis.read) != -1
+          bytes << byte.chr
+        end
+        bis.close
+        bytes
+      end
+
+      private
+      def resource
+        @resource ||= java.lang.Class.for_name('java.lang.String')
+        @resource
+      end
+    end
+   */
+   
 	public static void main(String[] args) throws NullPointerException,
 			IOException {
 		Ruby runtime = JavaEmbedUtils.initialize(new ArrayList());
